b5a31f9b3226226f23105f79177ecf7c314819f6
Administrator <siddhiseth3009@gmail.com>
2020-10-22 14:56:40 +0530

    [Siddhi]Add. AddToHead and display method added

--- LinkedListDemo/LinkedList.cs
+++ LinkedListDemo/LinkedList.cs
@@ -7,5 +7,30 @@ namespace LinkedListDemo
     class LinkedList
     {
         public Node head;
+        internal void AddToHead(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+                node.next = null;
+            else
+                node.next = this.head;
+            this.head = node;
+            Console.WriteLine("{0} is inserted at the head ", node.data);
+        }
+        public void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked List is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + "\t");
+                temp = temp.next;
+            }
+            Console.WriteLine("\n ______________________________________________________________________");
+        }
     }
 }
--- LinkedListDemo/Program.cs
+++ LinkedListDemo/Program.cs
@@ -7,6 +7,12 @@ namespace LinkedListDemo
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Linked List Demo");
+            //Adding new element to head LIFO
+            LinkedList list = new LinkedList();
+            list.AddToHead(70);
+            list.AddToHead(30);
+            list.AddToHead(56);
+            list.Display();
         }
     }
 }
