fe063cb0801b3ad52fbdf9a4a0f34737807605a8
Administrator <siddhiseth3009@gmail.com>
2020-10-22 14:44:51 +0530

    [Siddhi]Add. Welcome message added

---
+++ LinkedListDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinkedListDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Linked List Demo");
+        }
+    }
+}
